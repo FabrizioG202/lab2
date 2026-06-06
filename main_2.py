585ac0813ad7d7778353bcd3f24dbf92c85e40bb
Fabrizio Guidotti <fabrizio.guidotti.202@gmail.com>
2026-06-06 15:45:54 +0200

    Added code to get the embeddings from a sequence.

--- main_2.py
+++ main_2.py
@@ -1,61 +1,49 @@
-from sklearn.neural_network import MLPClassifier
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.model_selection import train_test_split
+import ankh
+import torch
 from tqdm import tqdm
-import Bio.SeqUtils.ProtParamData
-from Bio.SeqUtils.ProtParam import ProteinAnalysis
-import importlib
-import itertools
-import re
-from dataclasses import dataclass
-from typing import Iterable, Iterator, Any, Protocol, cast
-import sklearn.metrics
-import logomaker
-import numpy as np
-import polars as pl
-import sklearn.metrics
-import plotly.express as px
-import plotly.graph_objects as go
-import plotly.io as pio
-import contextlib
-import io
-from IPython.display import clear_output
-import src.data_collection; importlib.reload(src.data_collection); clear_output()  # noqa: E703, E702, E402 # fmt: skip
-import src.logo_generator; importlib.reload(src.logo_generator); clear_output()  # noqa: E703, E702, E402 # fmt: skip
-import src.utils.AdditionalProtParamData; importlib.reload(src.utils.AdditionalProtParamData); clear_output()  # noqa: E703, E702, E402 # fmt: skip
-import src.methods.von_heijne; importlib.reload(src.methods.von_heijne);  # noqa: E703, E702, E402 # fmt: skip
-import src.processing; importlib.reload(src.processing); clear_output()  # noqa: E703, E702, E402 # fmt: skip
-import src.graphics; importlib.reload(src.graphics); clear_output()  # noqa: E703, E702, E402 # fmt: skip
-import src.feature_extraction; importlib.reload(src.feature_extraction); clear_output()  # noqa: E703, E702, E402 # fmt: skip
-import src.methods.svm; importlib.reload(src.methods.svm); clear_output()  # noqa: E703, E702, E402 # fmt: skip
-import src.methods.mlp; importlib.reload(src.methods.mlp); clear_output()  # noqa: E703, E702, E402 # fmt: skip
-import src.methods.model_selection; importlib.reload(src.methods.model_selection); clear_output()  # noqa: E703, E702, E402 # fmt: skip
-import src.methods.von_heijne; importlib.reload(src.methods.von_heijne); clear_output()  # noqa: E703, E702, E402 # fmt: skip
-import src.feature_extraction; importlib.reload(src.feature_extraction); clear_output()  # noqa: E703, E702, E402 # fmt: skip
-import src.feature_importance; importlib.reload(src.feature_importance); clear_output()  # noqa: E703, E702, E402 # fmt: skip
+
+import src.data_collection
 
 # collect data
 all_positive, all_negative, positive, negative = src.data_collection.collect_data()
-import ankh
-import torch
 
 model, tokenizer = ankh.load_base_model()
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device
 
-protein_sequences = [
-    "MKALCLLLLPVLGLLVSSKTLCSMEEAINERIQEVAGSLIFRAISSIGLECQSVTSRGDLATCPRGFAVTGCTCGSACGSWDVRAETTCHCQCAGMDWTGARCCRVQPLEHHHHHH",
-    "GSHMSLFDFFKNKGSAATATDRLKLILAKERTLNLPYMEEMRKEIIAVIQKYTKSSDIHFKTLDSNQSVETIEVEIILPR",
-]
+model = model.to(device)  # ty:ignore[invalid-argument-type]
+model.eval()
 
-protein_sequences = [list(seq) for seq in protein_sequences]
 
-outputs = tokenizer(
-    protein_sequences,
-    add_special_tokens=True,
-    padding=True,
-    is_split_into_words=True,
-    return_tensors="pt",
-)
-with torch.no_grad():
-    embeddings = model(
-        input_ids=outputs["input_ids"], attention_mask=outputs["attention_mask"]
-    )
+def get_embeddings(sequences: list[str]):
+    tokenized_sequences = [list(seq) for seq in sequences]
+
+    outputs = tokenizer(
+        tokenized_sequences,
+        add_special_tokens=True,
+        padding=True,
+        is_split_into_words=True,
+        return_tensors="pt",
+    )  # ty:ignore[call-non-callable]
+
+    outputs = {k: v.to(device) for k, v in outputs.items()}
+
+    with torch.no_grad():
+        embeddings = model(
+            input_ids=outputs["input_ids"],
+            attention_mask=outputs["attention_mask"],
+        )
+
+    return embeddings
+
+
+positive_sequences = positive["sequence"].to_list()
+positive_embeddings = []
+
+
+# loop over the positive sequences in batches of 16 and get the embeddings for each batch
+for i in tqdm(range(0, len(positive_sequences), 16)):
+    batch_sequences = [s[:90] for s in positive_sequences[i : i + 4]]
+
+    batch_embeddings = get_embeddings(batch_sequences)
+    # positive_embeddings.append(batch_embeddings)
